3ffe316bba533e0e40f9bfb3c84f60c575687d1d
Ammar Arjomand <persiancookbook@gmail.com>
2018-09-06 12:06:36 +0100

    synchronous play

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-var http = require("http");
+// var http = require("http");
+//
+// http.createServer(function (request, response){
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Hello World\n');
+// }).listen(8081);
+//
+// console.log('Server up and running on port 8081');
 
-http.createServer(function (request, response){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8081);
 
-console.log('Server up and running on port 8081');
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("Program ended");

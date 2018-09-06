25cb71985de95fc4794b9549b11c040d4abb6ce2
Ammar Arjomand <persiancookbook@gmail.com>
2018-09-06 13:51:25 +0100

    Event-driven example

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// //CREATE SERVER
 // var http = require("http");
 //
 // http.createServer(function (request, response){
@@ -7,18 +8,35 @@
 //
 // console.log('Server up and running on port 8081');
 
-
+// //SYNCRONOUS BLOCKING EXAMPLE
 // var fs = require("fs");
 // var data = fs.readFileSync('input.txt');
 //
 // console.log(data.toString());
 // console.log("Program ended");
 
+// //ASYNCRONOUS NON-BLOCKING EXAMPLE
+// var fs = require("fs");
+// var data = fs.readFile('input.txt', function (err, data) {
+//   if (err) return console.error(err);
+//   console.log(data.toString());
+// });
+//
+// console.log("Program ended");
 
-var fs = require("fs");
-var data = fs.readFile('input.txt', function (err, data) {
-  if (err) return console.error(err);
-  console.log(data.toString());
+//EVENT-DRIVEN EXAMPLE
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+var connectHandler = function connected(){
+  console.log('connection succesful');
+  eventEmitter.emit('data_received');
+}
+
+eventEmitter.on('connection', connectHandler);
+
+eventEmitter.on('data_received', function(){
+  console.log('data received succesfully.');
 });
 
-console.log("Program ended");
+eventEmitter.emit('connection');
+console.log("Program Ended.");

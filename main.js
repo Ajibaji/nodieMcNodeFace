608788df3e15dd1a49a4734f556582c47a4e675a
Ammar Arjomand <persiancookbook@gmail.com>
2018-09-06 12:15:01 +0100

    asynchronous, non-blocking play

--- main.js
+++ main.js
@@ -8,8 +8,17 @@
 // console.log('Server up and running on port 8081');
 
 
-var fs = require("fs");
-var data = fs.readFileSync('input.txt');
+// var fs = require("fs");
+// var data = fs.readFileSync('input.txt');
+//
+// console.log(data.toString());
+// console.log("Program ended");
 
-console.log(data.toString());
+
+var fs = require("fs");
+var data = fs.readFile('input.txt', function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
+
 console.log("Program ended");
